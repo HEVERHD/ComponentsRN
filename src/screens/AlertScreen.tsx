72f83bccff5f471498418170d67947d7affe7df3
HEVERHD <hever11.hdgd@gmail.com>
2023-01-02 17:28:25 -0500

    Promt mesagge

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -2,6 +2,7 @@ import { View, Button, Alert } from 'react-native';
 import React from 'react';
 import HeaderTitle from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
+import prompt from 'react-native-prompt-android';
 
 export default function AlertScreen() {
     const showAlert = () => {
@@ -15,11 +16,40 @@ export default function AlertScreen() {
         ]);
     };
 
+    const showPrompt = () => {
+        prompt(
+            'Enter password',
+            'Enter your password to claim your $1.5B in lottery winnings',
+            [
+                {
+                    text: 'Cancel',
+                    onPress: () => console.log('Cancel Pressed'),
+                    style: 'cancel',
+                },
+                {
+                    text: 'OK',
+                    onPress: password =>
+                        console.log('OK Pressed, password: ' + password),
+                },
+            ],
+            {
+                type: 'secure-text',
+                cancelable: false,
+                defaultValue: 'test',
+                placeholder: 'placeholder',
+            },
+        );
+    };
+
     return (
         <View style={styles.globalMargin}>
             <HeaderTitle title="Alerts" />
 
             <Button title="Mostrar Alerta" onPress={showAlert} />
+
+            <View style={{ height: 10 }} />
+
+            <Button title="Mostrar Prompt" onPress={showPrompt} />
         </View>
     );
 }
