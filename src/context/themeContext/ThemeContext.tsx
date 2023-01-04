b0ff9cb5e5d0f61ededb481fc27ec219032bd4b9
HEVERHD <hever11.hdgd@gmail.com>
2023-01-03 20:20:52 -0500

    finish ContextTheme

--- src/context/themeContext/ThemeContext.tsx
+++ src/context/themeContext/ThemeContext.tsx
@@ -1,5 +1,11 @@
-import { createContext, useReducer } from 'react';
-import { LigthTheme, ThemeState, themeReducer } from './themeReducer';
+import { createContext, useReducer, useEffect } from 'react';
+import {
+    DarkTheme,
+    LigthTheme,
+    ThemeState,
+    themeReducer,
+} from './themeReducer';
+import { AppState, Appearance, useColorScheme } from 'react-native';
 
 interface ThemeContextProps {
     theme: ThemeState;
@@ -11,7 +17,26 @@ export const ThemeContext = createContext({} as ThemeContextProps);
 
 // el proovedor del themeContext
 export const ThemeProvider = ({ children }: any) => {
-    const [theme, dispatch] = useReducer(themeReducer, LigthTheme); // Leer todo el tema GLOBAL
+    const colorScheme = useColorScheme();
+
+    const [theme, dispatch] = useReducer(
+        themeReducer,
+        colorScheme === 'dark' ? DarkTheme : LigthTheme,
+    ); // Leer todo el tema GLOBAL
+
+    // useEffect(() => {
+    //     colorScheme === 'light' ? setLigthTheme() : setDarkTheme;
+    // }, [colorScheme]);
+
+    useEffect(() => {
+        AppState.addEventListener('change', status => {
+            if (status === 'active') {
+                Appearance.getColorScheme() === 'light'
+                    ? setLigthTheme()
+                    : setDarkTheme;
+            }
+        });
+    }, []);
 
     const setDarkTheme = () => {
         dispatch({ type: 'setDark_theme' });
